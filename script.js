c5d1ab6272c7332af5ab95308d154d11a505b21c
Dhruv Singh Tomar <tomardhruv628@gmail.com>
2024-08-23 00:22:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let currentplayer = "X";
+
+let arr = Array(9).fill(null);
+
+function checkWinner (){
+    if(
+        (arr[0] !== null && arr[0] == arr[1] && arr[1] == arr[2]) ||
+        (arr[3] !== null && arr[3] == arr[4] && arr[4] == arr[5]) ||
+        (arr[6] !== null && arr[6] == arr[7] && arr[7] == arr[8]) ||
+        (arr[0] !== null && arr[0] == arr[3] && arr[3] == arr[6]) ||
+        (arr[1] !== null && arr[1] == arr[4] && arr[4] == arr[7]) ||
+        (arr[2] !== null && arr[2] == arr[5] && arr[5] == arr[8]) ||
+        (arr[0] !== null && arr[0] == arr[4] && arr[4] == arr[8]) ||
+        (arr[2] !== null && arr[2] == arr[4] && arr[4] == arr[6]) 
+    ) {
+           document.write(`winner is ${currentplayer} `);
+           return;
+    }
+
+    if (!arr.some((e) => e === null)) {
+        document.write(`draw!!`)
+        return;
+    }
+        
+    
+}
+
+function handleclick(el){
+    const id = Number(el.id);
+    if (arr[id] !== null) return; 
+    arr[id] = currentplayer;
+    el.innerText = currentplayer;
+    checkWinner();
+    currentplayer = currentplayer === "x" ? "0" : "x" ; 
+}
+
+
